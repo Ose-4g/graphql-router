bfbd2cae3c173f6d16470ef8bec8f6cc8765cca2
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-04-23 22:05:15 +0100

    feat: fix bugs in Router class.

--- src/Router.ts
+++ src/Router.ts
@@ -19,13 +19,18 @@ export class Router {
     const allMiddlewares = this.middlewares.concat(middlewares);
     const n = allMiddlewares.length;
     let pos = 0;
-    let newResolver = (parent: any, args: any, context: any, info: GraphQLResolveInfo) => {
+    let newResolver = async (parent: any, args: any, context: any, info: GraphQLResolveInfo) => {
+      let main: any;
       const next: Next = async (error?: Error) => {
         if (error) throw error;
         else if (pos < n) await allMiddlewares[pos++](parent, args, context, info, next);
-        else if (graphQLField.resolve) graphQLField.resolve(parent, args, context, info);
+        else if (graphQLField.resolve) {
+          main = await graphQLField.resolve(parent, args, context, info);
+        }
       };
-      next();
+      await next();
+      pos = 0;
+      return main;
     };
 
     return {
--- src/__test__/mocks/mockApp.ts
+++ src/__test__/mocks/mockApp.ts
@@ -1,23 +1,55 @@
 import express, { Request, Response } from 'express';
-import { GraphQLSchema, GraphQLObjectType } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLResolveInfo, GraphQLFieldConfig } from 'graphql';
 import { graphqlHTTP } from 'express-graphql';
+import { Middleware, Next, Router } from '../../Router';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const createMiddleware = function (val: string): Middleware {
+  return (parent: any, args: any, context: any, info: GraphQLResolveInfo, next: Next) => {
+    console.log(val, 'started');
+    return next();
+    console.log(val, 'ended');
+  };
+};
+
+const router = new Router();
+router.use(createMiddleware('pre-first'));
+const helloResolver: GraphQLFieldConfig<any, any, any> = {
+  type: GraphQLString,
+  args: {},
+  resolve(parent, args, context, info) {
+    console.log('\n\n');
+    return 'My name is Ose4g';
+  },
+};
+
+const randomResolver: GraphQLFieldConfig<any, any, any> = {
+  type: GraphQLString,
+  args: {},
+  resolve(parent, args, context, info) {
+    console.log('\n\n');
+    return 'My name is random' + String(Date.now());
+  },
+};
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  fields: {},
+  fields: {
+    hello: router.add(helloResolver, createMiddleware('first'), createMiddleware('second')),
+    random: router.add(randomResolver, createMiddleware('lastone')),
+  },
 });
 
-const Mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {},
-});
+// const Mutation = new GraphQLObjectType({
+//   name: 'Mutation',
+//   fields: {},
+// });
 
-const schema: GraphQLSchema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
+const schema: GraphQLSchema = new GraphQLSchema({ query: RootQuery });
 
 app.use('/graphql', (req: Request, res: Response) => {
   return graphqlHTTP({
@@ -26,4 +58,7 @@ app.use('/graphql', (req: Request, res: Response) => {
   })(req, res);
 });
 
+app.listen(3098, () => {
+  console.log('graphql server started');
+});
 export { app };
